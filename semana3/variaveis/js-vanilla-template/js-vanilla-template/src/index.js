a9ca87411ae9509db1bf9077ae8339a0ab4d2883
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-22 12:39:13 -0300

    Exercício 1 de escrita de código finalizado.

--- semana3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
@@ -11,3 +11,23 @@
    atribuições para cada variável.
 */
 
+let nome;
+let idade;
+
+// console.log(typeof nome, typeof idade);
+
+/* O tipo impresso foi "undefined" para ambas as variáveis
+porque elas foram declaradas sem um valor, então
+não é possível saber qual o tipo delas. */
+
+nome = prompt("Qual o seu nome?");
+idade = prompt("Qual a sua idade?");
+
+// console.log(typeof nome, typeof idade);
+
+/* Quando o usuário digita valores, são atribuídos 
+"strings" para as variáveis, o número também é reconhecido
+como "string" e agora o typeof das variáveis muda de
+"undefined" para "string". */
+
+console.log("Olá", nome, ",", "você tem", idade, "anos.")
