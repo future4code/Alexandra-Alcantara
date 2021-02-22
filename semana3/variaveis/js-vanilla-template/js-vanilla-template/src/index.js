9cdca7aa91f70888769ed5adddb1ffc0d55b3a9f
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-22 12:04:06 -0300

    Exercícios de interpretação finalizados.

---
+++ semana3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,13 @@
+/* Exercícios de interpretação de código:
+
+1. No console.log(b) será imprimido o número "10"
+   Depois é atribuído um novo valor à letra b e então o 
+   console.log(a, b) irá imprimir os números "10, 5".
+
+2. São atribuídos números apenas às variáveis "a" e "b",
+   a variável "c" tem seu valor dado de acordo com o 
+   valor de "a", então console.log(a, b, c) irá imprimir 
+   os números "20, 10, 10", valores relacionados às últimas
+   atribuições para cada variável.
+*/
+
