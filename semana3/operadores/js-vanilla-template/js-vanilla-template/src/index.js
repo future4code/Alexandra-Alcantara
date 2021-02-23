ed1b4e949b2f8385f52eb369316a9bde582c167b
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-23 19:57:28 -0300

    Desafio 1 finalizado.

--- semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
@@ -83,11 +83,72 @@ listaDeTarefas.splice((prompt("Insira um índice de 0 a 2: ")), 1);
 console.log(listaDeTarefas); */
 
 // .4 //
-let nomeDoUsuario;
+/* let nomeDoUsuario;
 let emailDoUsuario;
 
 nomeDoUsuario = prompt("Qual o seu nome?");
 emailDoUsuario = prompt("Qual o seu e-mail?");
 
-console.log("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + "!");
+console.log("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + "!"); */
 
+// DESAFIOS //
+
+let grausCelsius;
+let grausFahrenheit;
+let kelvin;
+
+grausCelsius = Number(grausCelsius);
+grausFahrenheit = Number(grausFahrenheit);
+kelvin = Number(kelvin);
+
+// a. //
+// grausFahrenheit = 77;
+// let fahrenheitParaKelvin = (grausFahrenheit - 32) * (5/9) + 273.15;
+// console.log(grausFahrenheit + "°F é igual a " + fahrenheitParaKelvin + "K.");
+
+// b. //
+// grausCelsius = 80;
+// let celsiusParaFahrenheit = (grausCelsius * 1.8) + 32;
+// console.log(grausCelsius + "°C é igual a " + celsiusParaFahrenheit + "°F.");
+
+// c. //
+// grausCelsius = 30;
+// let celsiusParaFahrenheit = (grausCelsius * 1.8) + 32;
+// let celsiusParaKelvin = grausCelsius + 273;
+// console.log(grausCelsius + "°C é igual a " + celsiusParaFahrenheit + "°F e " + celsiusParaKelvin + "K.");
+
+// d. //
+// grausCelsius = Number(prompt("Insira graus Celsius para converter em Fahrenheit e Kelvin: "));
+// let celsiusParaFahrenheit = (grausCelsius * 1.8) + 32;
+// let celsiusParaKelvin = grausCelsius + 273;
+// console.log(grausCelsius + "°C é igual a " + celsiusParaFahrenheit + "°F e " + celsiusParaKelvin + "K.");
+
+// Converter de °C para °F:
+// grausCelsius = prompt("Insira graus Celsius para converter em Fahrenheit: ");
+// let celsiusParaFahrenheit = (grausCelsius * 1.8) + 32;
+// console.log(grausCelsius + "°C é igual a " + celsiusParaFahrenheit + "°F.");
+
+// Converter de °F para °C:
+// grausFahrenheit = prompt("Insira graus Fahrenheit para converter em Celsius: ");
+// let fahrenheitParaCelsius = (grausFahrenheit - 32) / 1.8;
+// console.log(grausFahrenheit + "°F é igual a " + fahrenheitParaCelsius + "°C.");
+
+// Converter de °F para K:
+// grausFahrenheit = prompt("Insira graus Fahrenheit para converter em Kelvin: ");
+// let fahrenheitParaKelvin = (grausFahrenheit - 32) * (5/9) + 273.15;
+// console.log(grausFahrenheit + "°F é igual a " + fahrenheitParaKelvin + "K.");
+
+// Converter de K para °F:
+// kelvin = prompt("Insira Kelvin para converter em Fahrenheit: ");
+// let kelvinParaFahrenheit = (kelvin - 273) * 1.8 + 32;
+// console.log(kelvin + "K é igual a " + kelvinParaFahrenheit + "°F.");
+
+// Converter de K para °C:
+// kelvin = prompt("Insira Kelvin para converter em Celsius: ");
+// let kelvinParaCelsius = kelvin - 273;
+// console.log(kelvin + "K é igual a " + kelvinParaCelsius + "°C.");
+
+// Converter de °C para K:
+// grausCelsius = Number(prompt("Insira graus Celsius para converter em Kelvin: "));
+// let celsiusParaKelvin = grausCelsius + 273;
+// console.log(grausCelsius + "°C é igual a " + celsiusParaKelvin + "K.");
