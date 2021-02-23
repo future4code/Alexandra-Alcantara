3384b646d27e830da24aadc0be6bccdb1218844e
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-23 12:21:39 -0300

    Exercício 2 de interpretação de código finalizado.

--- semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
@@ -18,3 +18,24 @@ console.log("c. ", resultado) // c. false
 console.log("e. ", typeof resultado) // d. boolean
 */
 
+// 2. //
+
+/* let array
+console.log('a. ', array) // a. undefined
+
+array = null
+console.log('b. ', array) // b. null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length) // c. 11
+
+let i = 0
+console.log('d. ', array[i]) // d. Erro?
+
+array[i+1] = 19
+console.log('e. ', array) // e. Não sei
+
+const valor = array[i+6]
+console.log('f. ', valor) // f. array[i+6]
+*/
+
