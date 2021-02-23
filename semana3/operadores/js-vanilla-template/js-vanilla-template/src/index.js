a199724c6eb89197c6c33413d43acdf6f7403bca
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-23 12:15:09 -0300

    Exercício 1 de interpretação de código finalizado.

---
+++ semana3/operadores/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,20 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO //
+
+// 1. //
+
+/* const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2  
+console.log("a. ", resultado) // a. false
+
+resultado = bool1 && bool2 && bool3
+console.log("b. ", resultado) // b. true
+
+resultado = !resultado && (bool1 || bool1)
+console.log("c. ", resultado) // c. false
+
+console.log("e. ", typeof resultado) // d. boolean
+*/
+
