fb095d1196f13f4bba8f6c6fb261d80884a4162f
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-24 16:16:31 -0300

    Exercícios de escrita de código finalizados.

--- semana3/condicionais/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/condicionais/js-vanilla-template/js-vanilla-template/src/index.js
@@ -40,3 +40,76 @@
 
 
 // EXERCÍCIOS DE ESCRITA DE CÓDIGO //
+
+// 4. //
+
+// Declaração da variável e conversão para número
+/* const idade = Number(prompt("Qual a sua idade?")); */
+
+// Condicional que checa em qual condição se enquadra a idade
+/* if (idade >= 18) {
+    console.log("Você pode dirigir.");
+}
+else {
+    console.log("Você não pode dirigir.");
+} */
+
+// 5. //
+
+// Declaração da variável
+//const turno = prompt("Qual o turno que você estuda? informe M para matutino, V para vespertino e N para noturno");
+
+// Condicional que checa o input do usuário e retorna
+// uma mensagem personalizada de acordo com o turno
+/*if (turno === "M" || turno === "m") {
+    console.log("Bom dia!");
+}
+else if (turno === "V" || turno === "v") {
+    console.log("Boa tarde!");
+}
+else if (turno === "N" || turno === "n") {
+    console.log("Boa noite!");
+}
+else {
+    console.log("Digite apenas M, V ou N.");
+}*/
+
+// 6. //
+
+/*const turnoSwitch = prompt("Qual o turno que você estuda? informe M para matutino, V para vespertino e N para noturno");
+
+switch (turnoSwitch) {
+    case 'M':
+        console.log("Bom dia!");
+        break
+    case 'm':
+        console.log("Bom dia!");
+        break
+    case 'V':
+        console.log("Boa tarde!");
+        break
+    case 'v':
+        console.log("Boa tarde!");
+        break
+    case 'N':
+        console.log("Boa noite!");
+        break
+    case 'n':
+        console.log("Boa noite!");
+        break
+    default:
+        console.log("Digite apenas M, V ou N.");
+        break
+}*/
+
+// 7. //
+
+const generoFilme = prompt("Qual o gênero do filme que você vai assistir?");
+const preco = prompt("O preço do ingresso está abaixo de R$15.00? s/n");
+
+if (generoFilme == "fantasia" && preco == "s") {
+    console.log("Bom filme!");
+}
+else {
+    console.log("Escolha outro filme :(");
+}
