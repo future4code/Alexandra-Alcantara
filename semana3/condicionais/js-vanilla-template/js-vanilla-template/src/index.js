da82a878cffe54a34f8818f51dfe174a7b007a1a
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-24 15:19:38 -0300

    Exercícios de interpretação de código finalizados.

---
+++ semana3/condicionais/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,42 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO //
+
+// 1. //
+/* O código recebe um input do usuário com um número,
+   (em forma de string) depois converte para o tipo número,
+   e com o condicional "if" ele testa se o resto da divisão
+   do número por 2 é zero, retornando a mensagem de "Passou
+   no teste." (ou seja, é um número par), ou retorna a
+   mensagem "Não passou no teste." se o resto da divisão
+   não for zero, indicando que o número informado é ímpar. */
+
+// 2. //
+/* a) O código serve para informar o usuário do preço
+      de algumas frutas.
+
+    b) A mensagem impressa no console será:
+       "O preço da fruta maçã é R$ 2.25"
+
+    c) Na retirada do "break" de pêra, o restante do
+       bloco continua sendo executado, no caso o default.
+       Acredito que o valor na mensagem será o do default:
+       "O preço da fruta pêra é R$ 5" */
+
+// 3. //
+ /* a) A primeira linha declara uma variável global
+       e a converte para número ao mesmo tempo em que 
+       solicita um input do usuário.
+
+    b) Se o usuário digitar o número 10, a mensagem no
+       terminal será "Esse número passou no teste", mas
+       se ele digitar o número -10, então nenhuma mensagem
+       será retornada no terminal, pois não existe um "else"
+       para o caso de números menores que zero.
+       
+
+    c) Não vai aparecer erro no console, mas a variável
+       "mensagem" não vai aparecer se o console.log 
+       estiver fora do escopo de onde ela foi criada. */
+
+
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO //
