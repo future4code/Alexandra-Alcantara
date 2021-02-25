674938810783edeabff10424be5343302defad52
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-02-25 13:22:06 -0300

    Exercícios de interpretação de código e desafio finalizados.

---
+++ semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,22 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO //
+
+// 1. //
+
+/* O código está contando de 0 a 4 e vai imprimir esse
+   intervalo no terminal (0, 1, 2, 3, 4), cada vez que 
+   a execução passa pelo "for", é acrescentado +1 à 
+   variável "valor" e a iteração só continua enquanto
+   "i < 5". */
+
+// 2. //
+
+/* a) No console serão impressos os números do array 
+      maiores que 18: (19, 21, 23, 25, 27, 30).
+
+   b) Acessar o índice de cada elemento é possível usando
+      for...in
+ */
+
+ // DESAFIO DE INTERPRETAÇÃO DE CÓDIGO //
+
+ 
