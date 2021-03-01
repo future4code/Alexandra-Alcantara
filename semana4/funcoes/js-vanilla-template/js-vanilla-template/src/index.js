fecbfb0f06c93005808ef782eab9224fe92c2502
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-01 17:07:42 -0300

    Exercícios de escrita de código finalizados.

--- semana4/funcoes/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana4/funcoes/js-vanilla-template/js-vanilla-template/src/index.js
@@ -32,30 +32,132 @@
 
 /* a) */
 
+/*let dadosFunction = (array) => {
+    console.log(array);
+    return array.lenght;
+}
+
+const texto = dadosFunction(["Eu sou Alexandra", "tenho 31 anos", "moro em Fortaleza", "e sou estudante."]);
+console.log(texto);*/
 
 /* b) */
 
+/*let suasInfos = (nome, idade, cidade, estudante) => {
+    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e ${estudante} estudante.`);
+}
+
+let nome = "Alê";
+let idade = 31;
+let cidade = "Interlaken";
+let estudante = 1;
+
+if (estudante) {
+    estudante = "sou";
+}
+else {
+    estudante = "não sou";
+}
+
+console.log(suasInfos(nome, idade, cidade, estudante));*/
 
 // 5. //
 
 /* a) */
 
+/*function sumNumbers(num1,num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+console.log(sumNumbers(20,11))*/
 
 /* b) */
  
+/*function maiorOuIgual(num1, num2) {
+    if (num1 >= num2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(maiorOuIgual(20, 11));*/
 
 /* c) */
 
+/*function recebeMensagem (texto) {
+    for (let i = 0; i < 10; i++) {
+        console.log(texto);
+    }
+}
+
+recebeMensagem("It's me 10 times!");*/
 
 // 6. //
 
 /* a) */
 
+/*const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22];
+
+function recebeNumeros (arrayNumber) {
+    console.log(`Este array contém ${arrayNumber.length} números`);
+}
+
+recebeNumeros(array);*/
 
 /* b) */
 
+function verificaSeEhPar (num) {
+    if (num % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+/*num = prompt("Que número gostaria de verificar?")
+console.log(`O número ${num} é par?`, recebeNumeroDeNovo(num));*/
 
 /* c) */
 
+/*arrayPares = [];
+arrayImpares = [];
+numerosAleatorios = [2, 5, 8, 7, 1, 6, 9];
+
+function contarNumPares (arrayNumeros) {
+    for (thing of arrayNumeros) {
+        if (thing % 2 == 0) {
+            arrayPares.push(arrayNumeros);
+        }
+        else {
+            arrayImpares.push(arrayNumeros);
+        }
+    }
+    console.log(`O array ${numerosAleatorios} possui`, arrayPares.length, "números pares.");
+    console.log(`O array ${numerosAleatorios} possui`, arrayImpares.length, "números ímpares.");
+}
+
+contarNumPares(numerosAleatorios);*/
 
 /* d) */
+
+arrayPares = [];
+arrayImpares = [];
+numerosAleatorios = [2, 5, 8, 7, 1, 6, 9];
+
+function contarNumPares (arrayNumeros) {
+    for (thing of arrayNumeros) {
+        if (verificaSeEhPar (thing)) {
+            arrayPares.push(arrayNumeros);
+        }
+        else {
+            arrayImpares.push(arrayNumeros);
+        }
+    }
+    console.log(`O array ${numerosAleatorios} possui`, arrayPares.length, "números pares.");
+    console.log(`O array ${numerosAleatorios} possui`, arrayImpares.length, "números ímpares.");
+} 
+
+contarNumPares(numerosAleatorios);
