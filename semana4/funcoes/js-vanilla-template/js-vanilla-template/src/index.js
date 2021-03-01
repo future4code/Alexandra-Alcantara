575134b32d8722b266baacee98a4b56688796fe6
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-01 12:24:02 -0300

    Exercícios de interpretação de código finalizados.

---
+++ semana4/funcoes/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,61 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO //
+
+// 1. //
+
+/* a) Serão impressos os números 10 e 50.
+
+   b) Acredito que como o "console.log" não faz parte da
+      função, nada seria impresso na tela, a função é 
+      chamada, mas não impressa.
+*/
+
+// 2. //
+
+/* a) Pelo laço, acredito que serão impressos apenas os
+      nomes nas posições 0 e 1 (Darvas e Caio).
+
+   b) Com a mudança, a impressão seria "Amanda e Caio".
+*/
+
+// 3. //
+
+/* O código verifica se um número é par, e se for, 
+   ele eleva esse número à segunda potência  e o adiciona
+   no arrayFinal, que também poderia ser chamado de 
+   arraySegundaPotencia. No final o arraySegundaPotencia
+   é retornado com return. */
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO //
+
+
+// 4. //
+
+/* a) */
+
+
+/* b) */
+
+
+// 5. //
+
+/* a) */
+
+
+/* b) */
+ 
+
+/* c) */
+
+
+// 6. //
+
+/* a) */
+
+
+/* b) */
+
+
+/* c) */
+
+
+/* d) */
