d828a038fe148e3d5eb00426efa8c1a4eec59197
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-03 18:13:40 -0300

    Funcionalidade para adicionar imagem implementada.

--- semana4/objetos/blog/blog-boilerplate/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/blog-boilerplate/index.js
@@ -2,6 +2,7 @@
 const titulo = document.getElementById("titulo-post");
 const autor = document.getElementById("autor-post");
 const conteudo = document.getElementById("conteudo-post");
+let imagem = document.getElementById("imagem-post");
 
 // Criando uma variável para o container que receberá o post
 const container = document.getElementById("container-de-posts");
@@ -13,13 +14,21 @@ function criarPost() {
     const post = {
         tituloDoPost: titulo.value,
         autorDoPost: autor.value,
-        conteudoDoPost: conteudo.value
+        conteudoDoPost: conteudo.value,
+        imagemDoPost: imagem.value
     }
 
     if (post.tituloDoPost != "" && post.autorDoPost != "" && post.conteudoDoPost != "") {
+        if (imagem !== "" && imagem.value.includes(".jpg") || imagem.value.includes(".png") || imagem.value.includes(".jpeg")) {
+        }
+      
+        else {
+            alert("Você não adicionou uma imagem ou ela não está no formato jpg ou png.")
+        }
         arrayDePosts.push(post);
         container.innerHTML += `<div>
                                     <h1>${post.tituloDoPost}</h1>
+                                    <img src="${post.imagemDoPost}" alt"">
                                     <p>${post.autorDoPost}</p>
                                     <p>${post.conteudoDoPost}</p>
                                 </div>`
@@ -27,6 +36,7 @@ function criarPost() {
         titulo.value = "";
         autor.value = "";    
         conteudo.value = "";
+        imagem.value = "";
     }
     else {
         alert("Por favor, preencha todos os campos!");
