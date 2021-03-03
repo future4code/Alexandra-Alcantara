3200dd8518050becca56c8ce07c7bf8a92723d5f
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-03 16:25:06 -0300

    Blog finalizado(falta desafio).

--- semana4/objetos/blog/blog-boilerplate/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/blog-boilerplate/index.js
@@ -4,11 +4,31 @@ const autor = document.getElementById("autor-post");
 const conteudo = document.getElementById("conteudo-post");
 
 // Criando uma variável para o container que receberá o post
-const container = document.getElementById("container-de-post");
+const container = document.getElementById("container-de-posts");
 
 // Criando o objeto para guardar as informações do post
-const posts = {
-    tituloDoPost: titulo,
-    autorDoPost: autor,
-    conteudoDoPost: conteudo
+const arrayDePosts = [];
+
+function criarPost() {
+    const post = {
+        tituloDoPost: titulo.value,
+        autorDoPost: autor.value,
+        conteudoDoPost: conteudo.value
+    }
+
+    if (post.tituloDoPost != "" && post.autorDoPost != "" && post.conteudoDoPost != "") {
+        arrayDePosts.push(post);
+        container.innerHTML += `<div>
+                                    <h1>${post.tituloDoPost}</h1>
+                                    <p>${post.autorDoPost}</p>
+                                    <p>${post.conteudoDoPost}</p>
+                                </div>`
+
+        titulo.value = "";
+        autor.value = "";    
+        conteudo.value = "";
+    }
+    else {
+        alert("Por favor, preencha todos os campos!");
+    }  
 }
