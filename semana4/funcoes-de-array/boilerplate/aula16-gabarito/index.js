dec7c73565e4586721562d6f545aa671e6b0571e
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-05 00:32:18 -0300

    Finalizado! (iuhuuu)

--- semana4/funcoes-de-array/boilerplate/aula16-gabarito/index.js
+++ semana4/funcoes-de-array/boilerplate/aula16-gabarito/index.js
@@ -1,39 +1,51 @@
-let arrDespesas = []
-imprimirDespesas(arrDespesas)
-imprimirExtrato()
-
+let arrDespesas = [];
+imprimirDespesas(arrDespesas);
+imprimirExtrato();
 
 // PRIMEIRO
-function imprimirDespesas(despesas){
+function imprimirDespesas(despesas) {
     let divDespesas = document.getElementById('despesas')
     divDespesas.innerHTML = '<p><u>Despesas Detalhadas</u></p>'
 
-      //AQUI VEM A IMPLEMENTAÇÃO
-      despesas.forEach((despesa) => {
+    //AQUI VEM A IMPLEMENTAÇÃO
+    despesas.forEach((despesa) => {
         divDespesas.innerHTML += `<p>valor: R$ ${despesa.valor} | 
                                      tipo: ${despesa.tipo} | 
                                      descrição: ${despesa.descricao}</p>`
     })
 }
-  
-
-
 
 // SEGUNDO 
-function imprimirExtrato(){
+function imprimirExtrato() {
     let divExtrato = document.getElementById('extrato')
     let gastoTotal = 0
     let gastoAlimentacao = 0
     let gastoUtilidades = 0
     let gastoViagem = 0
 
+    arrDespesas.forEach((despesa) => {
+        switch (despesa.tipo) {
+            case 'alimentacao':
+                gastoAlimentacao = gastoAlimentacao + despesa.valor;
+                break;
+            case 'utilidades':
+                gastoUtilidades = gastoUtilidades + despesa.valor;
+                break;
+            case 'viagem':
+                gastoViagem = gastoViagem + despesa.valor;
+                break;
+            default:
+                break;
+        }
+        gastoTotal = gastoTotal + despesa.valor;
+    })
 
     // AQUI VEM A IMPLEMENTAÇÃO
 
     divExtrato.innerHTML = `<p>Extrato: Gasto Total: R$${gastoTotal} | Alimentação: R$${gastoAlimentacao} | 
-                                        Utilidades: R$${gastoUtilidades} | Viagem: R$${gastoViagem}</p>`
-}
+                                            Utilidades: R$${gastoUtilidades} | Viagem: R$${gastoViagem}</p>`
 
+}
 
 function limparFiltros() {
     document.getElementById('tipoFiltro').value = ""
@@ -41,14 +53,12 @@ function limparFiltros() {
     document.getElementById('valorFiltroMax').value = ""
 }
 
-
-
-function adicionarDespesa(){
+function adicionarDespesa() {
     let valorCdt = document.getElementById('valorCadastro')
     let tipoCtd = document.getElementById('tipoCadastro')
     let descricaoCtd = document.getElementById('descricaoCadastro')
 
-    if(validarValor(valorCdt) && validarTipo(tipoCtd) && validarDescricao(descricaoCtd)){
+    if (validarValor(valorCdt) && validarTipo(tipoCtd) && validarDescricao(descricaoCtd)) {
         let novaDespesa = {
             valor: Number(valorCdt.value),
             tipo: tipoCtd.value,
@@ -56,12 +66,12 @@ function adicionarDespesa(){
         }
 
         arrDespesas.push(novaDespesa)
-        
+
         valorCdt.value = ""
         tipoCtd.value = ""
         descricaoCtd.value = ""
 
-        
+
         limparFiltros()
         imprimirDespesas(arrDespesas)
         imprimirExtrato()
@@ -70,46 +80,41 @@ function adicionarDespesa(){
     }
 }
 
-
-
 // TERCEIRO
-function filtrarDespesas(){
+function filtrarDespesas() {
     let tipoFiltro = document.getElementById('tipoFiltro').value
     let valorMin = Number(document.getElementById('valorFiltroMin').value)
     let valorMax = Number(document.getElementById('valorFiltroMax').value)
-
-
-    let despesasFiltradas // AQUI NESSA VARIÁVEL VEM A IMPLEMENTAÇÃO
+    
+    let despesasFiltradas = arrDespesas.filter((despesa) => {
+        return despesa.tipo === tipoFiltro &&
+            (despesa.valor >= valorMin &&
+                despesa.valor <= valorMax);
+    })  // AQUI NESSA VARIÁVEL VEM A IMPLEMENTAÇÃO
 
     imprimirDespesas(despesasFiltradas)
 }
-
-
-
-
-
-
 
 // FunÇoes que fazem validaÇoes dos inputs de criaÇao de despesas 
 
 // NÃO SE PREOCUPEM EM ENTENDER ESSAS FUNÇÕES
 
-function validarValor(valor){
-    if(valor.value.length > 0 && parseInt(valor.value) > 0){
+function validarValor(valor) {
+    if (valor.value.length > 0 && parseInt(valor.value) > 0) {
         return true
     }
     return false
 }
 
-function validarTipo(tipo){
-    if(tipo.value !== ""){
+function validarTipo(tipo) {
+    if (tipo.value !== "") {
         return true
     }
     return false
 }
 
-function validarDescricao(texto){
-    if(texto.value.replace(/ /g,"").length !== 0){
+function validarDescricao(texto) {
+    if (texto.value.replace(/ /g, "").length !== 0) {
         return true
     }
     return false
