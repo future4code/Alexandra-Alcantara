bf64ae8f41bb67d818266328bf4e42ed648d502a
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-11 23:44:34 -0300

    Implementação 2: done

--- semana5/estado-e-comp-de-classe/insta4/insta4 2/src/App.js
+++ semana5/estado-e-comp-de-classe/insta4/insta4 2/src/App.js
@@ -5,7 +5,29 @@ import ravioli from './img/ravioli.jpeg';
 import raviolifolgada from './img/raviolifolgada.jpeg';
 import miniale from './img/miniale.jpeg';
 import bigale from './img/bigale.jpeg';
+import styled from "styled-components"
 
+const FormularioDoPost = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+`;
+
+const InputsDoFormulario = styled.input`
+  margin: 1px;
+  border-radius: 50px;
+  border: 1px solid #626262;
+  padding: 4px;
+`;
+
+const BotaoFormulario = styled.button`
+  background-color: #000080;
+  color: #FFFFFF;
+  border: none;
+  border-radius: 50px;
+  padding: 4px 8px;
+`;
 
 class App extends React.Component { // DECLARANDO UM COMPONENTE DE CLASSE
   state = {
@@ -25,9 +47,41 @@ class App extends React.Component { // DECLARANDO UM COMPONENTE DE CLASSE
         fotoUsuario: bigale,
         fotoPost: miniale
       }
-    ]
+    ],
+    valorInputFoto: "",
+    valorInputNome: "",
+    valorInputPost: ""
   } 
+
+  adicionaPost = () => {
+    const novoPost = {
+      fotoUsuario: this.state.valorInputFoto,
+      nomeUsuario: this.state.valorInputNome,
+      fotoPost: this.state.valorInputPost
+    };
+
+    const novaLista = [...this.state.posts, novoPost];
+
+    this.setState ({
+      posts: novaLista,
+      valorInputFoto: "",
+      valorInputNome: "",
+      valorInputPost: ""
+    });
+  };
   
+  onChangeInputFoto = (event) => {
+    this.setState({ valorInputFoto: event.target.value });
+  };
+
+  onChangeInputNome = (event) => {
+    this.setState({ valorInputNome: event.target.value });
+  };
+
+  onChangeInputPost = (event) => {
+    this.setState({ valorInputPost: event.target.value });
+  };
+
   render() {
     const listaDePosts = this.state.posts.map((post) => {
       return (
@@ -38,8 +92,27 @@ class App extends React.Component { // DECLARANDO UM COMPONENTE DE CLASSE
         />
       );
     });
+    
     return (
       <div className={'app-container'}>
+      <FormularioDoPost>
+        <InputsDoFormulario
+          value={this.state.valorInputFoto}
+          onChange={this.onChangeInputFoto}
+          placeholder={"Foto do Perfil"}/>
+
+        <InputsDoFormulario
+          value={this.state.valorInputNome}
+          onChange={this.onChangeInputNome}
+          placeholder={"Nome do Usuário"}/>
+
+        <InputsDoFormulario
+          value={this.state.valorInputPost}
+          onChange={this.onChangeInputPost}
+          placeholder={"Post"}/>
+
+        <BotaoFormulario onClick={this.adicionaPost}>Adicionar</BotaoFormulario>
+      </FormularioDoPost>
         {listaDePosts}
       </div>
     );
