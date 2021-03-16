cb0a2be9826ef5ab8ad4f3add47a4b020a3f6052
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-16 12:46:40 -0300

    Análise: done.

---
+++ semana6/ciclo-de-vida/boiler-plate/boiler-plate/src/App.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import styled from 'styled-components'
+import './styles.css'
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+`
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`
+
+export default class App extends React.Component {
+    state = {
+      tarefas: [
+        {
+          id: Date.now(),
+          texto: "regar as plantas",
+          completa: false
+        },
+        {
+          id: Date.now(),
+          texto: "Estudar",
+          completa: true
+        }
+      ],
+      inputValue: '',
+      filtro: ''
+    }
+
+  componentDidUpdate() {
+
+  };
+
+  componentDidMount() {
+
+  };
+
+  onChangeInput = (event) => {
+
+  }
+
+  criaTarefa = () => {
+
+  }
+
+  selectTarefa = (id) => {
+
+  }
+
+  onChangeFilter = (event) => {
+
+  }
+
+  render() {
+    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+      switch (this.state.filtro) {
+        case 'pendentes':
+          return !tarefa.completa // RETORNA AS TAREFAS QUE TEM "FALSE" NO "COMPLETA"
+        case 'completas':
+          return tarefa.completa
+        default:
+          return true
+      }
+    })
+
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <button onClick={this.criaTarefa}>Adicionar</button>
+        </InputsContainer>
+        <br/>
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+        </InputsContainer>
+        <TarefaList>
+          {listaFiltrada.map(tarefa => { // "TAREFA" É O NOME DO ARRAY
+            return (
+              <Tarefa
+                completa={tarefa.completa} // "TAREFA.COMPLETA" REPRESENTA O ITEM "COMPLETA" DO ARRAY "TAREFA"
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+            )
+          })}
+        </TarefaList>
+      </div>
+    )
+  }
+}
