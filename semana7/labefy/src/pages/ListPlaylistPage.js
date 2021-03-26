932288e3df500c65c720db661172e6e33a806a87
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-26 14:57:47 -0300

    Pagina com a lista de  playlists operacional

--- semana7/labefy/src/pages/ListPlaylistPage.js
+++ semana7/labefy/src/pages/ListPlaylistPage.js
@@ -1,11 +1,39 @@
+import axios from 'axios';
 import React from 'react';
+import { axiosConfig, baseUrl } from '../parameters';
 
 export default class ListPlaylistPage extends React.Component {
+    state = {
+        playlists: []
+    }
+
+    componentDidMount() {
+        this.getPlaylist()
+    }
+    
+    getPlaylist = () => {
+        axios.get(baseUrl, axiosConfig)
+        .then((res) => {
+            this.setState({ playlists: res.data.result.list })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+    
     render() {
         return(
             <div>
                 <button onClick={this.props.changePage}>Create a new playlist</button>
                 <h2>Playlists</h2>
+                {this.state.playlists.map((playlist) => {
+                    return(
+                        <div key={playlist.id}>
+                            <p>{playlist.name}</p>
+                        </div>
+                    )
+                }
+                )}
             </div>
         )
     }
