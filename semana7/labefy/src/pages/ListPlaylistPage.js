c2f775b0c58c06ed18ccf9830c474947a7d463a4
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-26 21:57:58 -0300

    Estilizaçao da lista de playlists concluida.

--- semana7/labefy/src/pages/ListPlaylistPage.js
+++ semana7/labefy/src/pages/ListPlaylistPage.js
@@ -1,6 +1,72 @@
 import axios from 'axios';
 import React from 'react';
 import { axiosConfig, baseUrl } from '../parameters';
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+    background-color: #d64c00;
+    border-radius: 10px;
+    width: 40vw;
+    height: fit-content;
+    margin: 0 auto;
+    padding: 6px;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+    color: #FFFFFF;
+`;
+
+const HeaderCreate = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #FFFFFF;
+`;
+
+const ButtonCreatePlaylist = styled.button`
+    background-color: #FFFFFF;
+    color: #d64c00;
+    border: none;
+    width: 8vw;
+    border-radius: 10px;
+    padding: 3px;
+    font-size: 10.5px;
+    cursor: pointer;
+    padding: 5px;
+
+    :hover {
+        background-color: #e76200;
+        color: #FFFFFF;
+        box-shadow: 2px 2px 5px #c78900;
+    }
+`;
+
+const PlaylistList = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #FFFFFF;
+`;
+
+const PlaylistNames = styled.p`
+    margin: 5px;
+    padding: 3px;
+`;
+
+const ButtonRemove = styled.button`
+    border: none;
+    color: #FFFFFF;
+    background-color: #d64c00;
+    font-size: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+    padding: 5px; 
+
+    :hover {
+        background-color: #FFFFFF;
+        color: #d64c00;
+    }
+`
 
 export default class ListPlaylistPage extends React.Component {
     state = {
@@ -35,19 +101,21 @@ export default class ListPlaylistPage extends React.Component {
     
     render() {
         return(
-            <div>
-                <button onClick={this.props.changePage}>Create a new playlist</button>
-                <h2>Playlists</h2>
+            <MainContainer>
+                <HeaderCreate>
+                    <h2>Playlists</h2>
+                    <ButtonCreatePlaylist onClick={this.props.changePage}>Create a new playlist</ButtonCreatePlaylist>
+                </HeaderCreate>
                 {this.state.playlists.map((playlist) => {
                     return(
-                        <div key={playlist.id}>
-                            <p>{playlist.name}</p>
-                            <button onClick={() => this.deletePlaylist(playlist.id)}>x</button>
-                        </div>
+                        <PlaylistList key={playlist.id}>
+                            <PlaylistNames>{playlist.name}</PlaylistNames>
+                            <ButtonRemove onClick={() => this.deletePlaylist(playlist.id)}>remove</ButtonRemove>
+                        </PlaylistList>
                     )
                 }
                 )}
-            </div>
+            </MainContainer>
         )
     }
 }
