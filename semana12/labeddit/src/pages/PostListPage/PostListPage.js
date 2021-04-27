8f5c3d47453c883785c5253d6fe2f238d29d39a3
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-04-27 18:19:00 -0300

    Botao para adicionar post implementado.

--- semana12/labeddit/src/pages/PostListPage/PostListPage.js
+++ semana12/labeddit/src/pages/PostListPage/PostListPage.js
@@ -2,11 +2,27 @@ import React, { useContext } from "react";
 import useProtectedPage from "../../hooks/useProtectedPage";
 import PostCard from "../../components/PostCard/PostCard";
 import GlobalStateContext from "../../global/GlobalStateContext";
-import { PostCardContainer, Title, Text } from "./styled";
-import { CardActionArea, CardMedia, Typography, Box } from "@material-ui/core";
+import {
+  PostCardContainer,
+  AddPostButton,
+  Text,
+  Title,
+  TitleContainer,
+} from "./styled";
+import {
+  CardActionArea,
+  CardMedia,
+  Typography,
+  Box,
+  Button,
+} from "@material-ui/core";
+import { goToAddPost } from "../../routes/coordinator";
+import { useHistory } from "react-router-dom";
+import { Add } from "@material-ui/icons";
 
 const PostListPage = () => {
   useProtectedPage();
+  const history = useHistory();
   const { states } = useContext(GlobalStateContext);
   console.log("Aqui estão os posts", states.posts);
 
@@ -14,14 +30,18 @@ const PostListPage = () => {
     states.posts &&
     states.posts.map((post) => {
       return (
-        <CardActionArea key={post.id}>
-          <Box m={2}>
-            <Typography color={"primary"} variant={"h6"}>
-              {post.title}
-            </Typography>
-          </Box>
-          <Text>{post.text}</Text>
-        </CardActionArea>
+        <Box width={"60vw"} border={"1px solid gray"} m={1} borderRadius={10}>
+          <CardActionArea key={post.id} onClick={null}>
+            <Box m={2}>
+              <Typography color={"primary"} variant={"h6"}>
+                {post.title}
+              </Typography>
+            </Box>
+            <Box>
+              <Text>{post.text}</Text>
+            </Box>
+          </CardActionArea>
+        </Box>
         // <PostCard key={post.id} title={states.title} text={states.text} />
       );
     });
@@ -29,11 +49,20 @@ const PostListPage = () => {
   return (
     <div>
       <Box mt={2} ml={2}>
-        <Typography color={"primary"} variant={"h5"}>
-          Popular Posts
-        </Typography>
+        <TitleContainer>
+          <Typography color={"primary"} variant={"h5"}>
+            Popular Posts
+          </Typography>
+          <Button
+            color={"primary"}
+            variant={"contained"}
+            onClick={() => goToAddPost(history)}
+          >
+            <Add />
+          </Button>
+        </TitleContainer>
       </Box>
-      {postCards}
+      <PostCardContainer>{postCards}</PostCardContainer>
     </div>
   );
 };
--- semana12/labeddit/src/pages/PostListPage/styled.js
+++ semana12/labeddit/src/pages/PostListPage/styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const PostCardContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const Title = styled.p`
@@ -13,3 +13,15 @@ export const Title = styled.p`
 export const Text = styled.p`
   padding: 0 50px 0 50px;
 `;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+// export const AddPostButton = styled(Fab)`
+//   position: fixed !important;
+//   right: 20px;
+//   bottom: 20px;
+//   z-index: 3;
+// `;
