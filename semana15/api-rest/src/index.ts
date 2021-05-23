44de80422e2b38c89f1304c55ccaaff83ba0b30b
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-05-23 14:12:16 -0300

    Exercicio 2: Done.

--- semana15/api-rest/src/index.ts
+++ semana15/api-rest/src/index.ts
@@ -1,24 +1,46 @@
 //npm run dev
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { users } from "./users";
+import { users, user } from "./users";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// Exercício 1
-// Endpoint: Get lista de usuários
-// a) Método: GET
-// b) A entidade será indicada pelo que se deseja obter da requisição,
-//    nesse caso é a lista de usuários, então a entidade será "/users".
+/* Exercício 1
+    Endpoint: Get lista de usuários
+    a) Método: GET
+    b) A entidade será indicada pelo que se deseja obter da requisição,
+       nesse caso é a lista de usuários, então a entidade será "/users". */
 
 app.get("/users", (req: Request, res: Response) => {
   res.status(200).send(users);
 });
 
-// Endpoint 2: Get
+/* Exercício 2
+    Endpoint: Get lista de usuários por type
+    a) O parâmetro de type foi passado como query pois a requisição receberia 
+       apenas um parâmetro para executar a pesquisa.
+    b) Sim, criando um "enum" específico para os tipos de usuário, assim a 
+       os types só podem usar o que foi definido no enum. */
+
+app.get("/users/search", (req: Request, res: Response) => {
+  let result: user[] = users;
+
+  if (req.query.type) {
+    const type = req.query.type as string;
+    result = result.filter((user) =>
+      user.type.toLowerCase().includes(type.toLowerCase())
+    );
+  }
+
+  if (req.query.type && result.length > 0) {
+    res.status(200).send(result);
+  } else {
+    res.status(404).send("Users not found :(");
+  }
+});
 
 // Endpoint 3: Get
 
