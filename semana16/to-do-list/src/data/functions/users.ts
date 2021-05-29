97b7ecc5de5c932794d7b10bf417ea8bb4a91f1c
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-05-29 02:55:45 -0300

    Endpoint 3: DONE.

--- semana16/to-do-list/src/data/functions/users.ts
+++ semana16/to-do-list/src/data/functions/users.ts
@@ -1,18 +1,21 @@
 import { connection } from "../connection";
 
+// Função do endpoint 1
 export const getUserById = async (id: string): Promise<any> => {
   const result = await connection.raw(`
-              SELECT id, nickname FROM ToDoListUser WHERE id = '${id}'
+              SELECT id, nickname FROM ToDoListUser 
+              WHERE id = '${id}'
           `);
   return result[0][0];
 };
 
+// Função do endpoint 2
 export const createUser = async (
   name: string,
   nickname: string,
   email: string
 ): Promise<any> => {
-  const result = await connection.raw(`
+  await connection.raw(`
     INSERT INTO ToDoListUser (name, nickname, email)
     VALUES(
         "${name}",
@@ -20,4 +23,20 @@ export const createUser = async (
         "${email}"
     )
 `);
+};
+
+// Função do endpoint 3
+export const editUser = async (
+  id: string,
+  name: string,
+  nickname: string,
+  email: string
+): Promise<any> => {
+  await connection("ToDoListUser")
+    .update({
+      name: name,
+      nickname: nickname,
+      email: email,
+    })
+    .where("id", id);
 };
--- semana16/to-do-list/src/router.ts
+++ semana16/to-do-list/src/router.ts
@@ -1,23 +1,41 @@
 import express, { Router, Request, Response } from "express";
-import { createUser, getUserById } from "./data/functions/users";
+import { createUser, editUser, getUserById } from "./data/functions/users";
 
 const routes: Router = express.Router();
 
+// Adicionar usuário (endpoint 1)
 routes.put("/user", async (req: Request, res: Response) => {
   try {
     const { name, nickname, email } = req.body;
-    const newUser = await createUser(name, nickname, email);
+    await createUser(name, nickname, email);
     res.status(200).send(`User Created!`);
-  } catch (error) {
-    res.status(400).send("Unexpected error.");
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
   }
 });
 
+// Buscar usuário pelo id (endpoint 2)
 routes.get("/user/:id", async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
     const user = await getUserById(id);
     res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+// Editar usuário indicando o id (endpoint 3)
+routes.post("/user/edit/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, nickname, email } = req.body;
+    await editUser(id, name, nickname, email);
+    res.status(200).send(`Updated!`);
   } catch (err) {
     res.status(400).send({
       message: err.message,
