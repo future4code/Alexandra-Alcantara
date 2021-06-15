7241253b31b2e6f0eb98d44bca0fda2a1026529a
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-06-15 16:37:10 -0300

    Exercicio 2: DONE.

--- semana19/routes/cruz-arquitetura-I/src/model/user.ts
+++ semana19/routes/cruz-arquitetura-I/src/model/user.ts
@@ -1,19 +1,27 @@
 export enum USER_ROLES {
-   NORMAL = 'NORMAL',
-   ADMIN = 'ADMIN'
+  NORMAL = "NORMAL",
+  ADMIN = "ADMIN",
 }
 
 export type authenticationData = {
-   id: string,
-   role: USER_ROLES
-}
+  id: string;
+  role: USER_ROLES;
+};
 
 export type userData = {
-   name: string,
-   nickname: string,
-   email: string,
-   password: string,
-   role: USER_ROLES
-}
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  role: USER_ROLES;
+};
 
-export type user = userData & { id: string }
+export type user = userData & { id: string };
+
+export type signUpInputDTO = {
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  role: USER_ROLES;
+};
