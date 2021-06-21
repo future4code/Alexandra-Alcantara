93cfc4a930c61b59e0e947a3ef702ce897226279
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-06-21 15:04:33 -0300

    Exercise 4: DONE.

--- semana20/tests/tests/performPurchase.test.ts
+++ semana20/tests/tests/performPurchase.test.ts
@@ -7,37 +7,29 @@ describe("Testing performPurchase function", () => {
       name: "Amelie",
       balance: 100,
     };
-
     const result = performPurchase(user, 40);
-
     expect(result).toEqual({
       name: "Amelie",
       balance: 60,
     });
   });
-
   test("Testing balance equal than value", () => {
     const user: User = {
       name: "Amelie",
       balance: 100,
     };
-
     const result = performPurchase(user, 100);
-
     expect(result).toEqual({
       name: "Amelie",
       balance: 0,
     });
   });
-
   test("Testing balance less than value", () => {
     const user: User = {
       name: "Amelie",
       balance: 100,
     };
-
     const result = performPurchase(user, 150);
-
     expect(result).toBeUndefined();
   });
 });
