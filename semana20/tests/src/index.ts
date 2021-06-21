c0eaf580c2bd28139126709ec962ad0d9ae59a96
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-06-21 12:48:55 -0300

    Exercise 1: DONE.

---
+++ semana20/tests/src/index.ts
@@ -0,0 +1,13 @@
+import knex from "knex";
+
+export const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    port: 3306,
+    multipleStatements: true,
+  },
+});
---
+++ semana20/tests/src/performPurchase.ts
@@ -0,0 +1,14 @@
+import { User } from "./model/user";
+
+export const performPurchase = (
+  user: User,
+  value: number
+): User | undefined => {
+  if (user.balance >= value) {
+    return {
+      ...user,
+      balance: user.balance - value,
+    };
+  }
+  return undefined;
+};
